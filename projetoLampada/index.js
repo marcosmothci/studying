ba1f75301aee2a5c559293e605d9af067ea9f8a6
Marcos Mothci Pereira <109672068+marcosmothci@users.noreply.github.com>
2022-10-05 05:16:13 -0300

    Add files via upload
    nova versão do arquivo JS. deixei o codigo antigo comentado, e o novo bonitão logo abaixo.

--- projetoLampada/index.js
+++ projetoLampada/index.js
@@ -1,32 +1,45 @@
 
-
-
-document.getElementById('acender').addEventListener('click', ()=> 
+/* document.getElementById('acender').addEventListener('click', ()=> 
 document.getElementById('lampada').src == 'http://i.stack.imgur.com/ybxlO.jpg'? 
 document.getElementById('lampada').src = 'http://i.stack.imgur.com/MRjsF.jpg' : 
 document.getElementById('lampada').src == 'http://i.stack.imgur.com/MRjsF.jpg' ?  
-alert('Tá quebrada, tem que arrumar') : document.getElementById('lampada').src = 'http://i.stack.imgur.com/ybxlO.jpg');
-
+alert('Tá quebrada, tem que arrumar') : document.getElementById('lampada').src = 'http://i.stack.imgur.com/ybxlO.jpg'); */
+/*
 document.getElementById('apagar').addEventListener('click', ()=> 
 document.getElementById('lampada').src == 'https://i.stack.imgur.com/b983w.jpg'?
 document.getElementById('lampada').src = 'http://i.stack.imgur.com/MRjsF.jpg' :
 document.getElementById('lampada').src == 'http://i.stack.imgur.com/MRjsF.jpg'?
-alert('Tá quebrada, tem que arrumar') : document.getElementById('lampada').src = 'https://i.stack.imgur.com/b983w.jpg');
-
-
+alert('Tá quebrada, tem que arrumar') : document.getElementById('lampada').src = 'https://i.stack.imgur.com/b983w.jpg'); */
+/*
 document.getElementById('arrumar').addEventListener('click', ()=>
 document.getElementById('lampada').src == 'http://i.stack.imgur.com/MRjsF.jpg'?
 document.getElementById('lampada').src = 'https://i.stack.imgur.com/b983w.jpg' : alert('nao tem o que arrumar'))
+*/
 
+const lamp = document.getElementById('lampada')
+const acesa = 'http://i.stack.imgur.com/ybxlO.jpg'
+const apagada = 'https://i.stack.imgur.com/b983w.jpg'
+const quebrada = 'http://i.stack.imgur.com/MRjsF.jpg'
 
-
-
-
-function quebraLampada() {
-    document.getElementById('lampada').src = 'http://i.stack.imgur.com/MRjsF.jpg'
+function mandaArrumar() {
+    alert('Tá quebrada, tem que mandar arrumar!')
 }
 
-setInterval(quebraLampada, 10500);
+function quebraLampada() {
+    lamp.src = 'http://i.stack.imgur.com/MRjsF.jpg'
+} 
+
+document.getElementById('acender').addEventListener('click', ()=> lamp.src == acesa?
+ lamp.src = quebrada : lamp.src == quebrada? mandaArrumar() : lamp.src = acesa)
+
+document.getElementById('apagar').addEventListener('click', ()=> lamp.src == apagada? 
+lamp.src = quebrada : lamp.src == quebrada? mandaArrumar() : lamp.src = apagada)
+
+document.getElementById('arrumar').addEventListener('click', ()=> lamp.src == quebrada? lamp.src = apagada : alert('Não tem nada pra arrumar aqui.'))
+
+
+
+setInterval(quebraLampada,Math.floor(Math.random() * 10000) + 3000);
 
 
 
